d585d0968f05b795b86a88b8c2befff5f513215f
SWillames <sergiowoc@gmail.com>
2019-08-26 01:36:46 -0300

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Coin.create(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+)
+
+Coin.create(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://d2a4hncphh3gxw.cloudfront.net/image/artwork/40311/89/23/89231ba1d7cd720fb5d00106b33bf9db_40311"
+)
+
+Coin.create(
+    description: "DASH",
+    acronym: "DASH",
+    url_image: "https://wiki.trezor.io/images/Dash.png"
+)
---
+++ lib/tasks/dev.rake
@@ -0,0 +1,24 @@
+namespace :dev do
+  desc "Configura o ambiente de desenvolvimento"
+  task setup: :environment do
+    if Rails.env.development?
+      show_spinner("Apagando BD...") {%x(rails db:drop)}
+         
+      show_spinner("Criando BD") {%x(rails db:create)}
+      
+      show_spinner("Migrando o BD") {%x(rails db:migrate)}
+
+      show_spinner("Populando o BD....") {%x(rails db:seed)}
+    else
+      puts "Você não está no ambiente de desenvolvimento"
+    end
+  end
+
+  def show_spinner(msg_start, msg_end = "Concluido")
+    spinner = TTY::Spinner.new(":[spinner) #{msg_start}", format: :dots_9)
+    spinner.auto_spin
+    yield
+    spinner.success("#{msg_end}")
+  end
+  
+end
