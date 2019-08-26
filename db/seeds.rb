79a9f4ef950d259753d6d4c235fc57dbdd87cf64
SWillames <sergiowoc@gmail.com>
2019-08-26 02:15:18 -0300

    refactoring the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas...", format: :dots_10)
+spinner.auto_spin
+coins = [
+          { 
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+          },   
+          {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://d2a4hncphh3gxw.cloudfront.net/image/artwork/40311/89/23/89231ba1d7cd720fb5d00106b33bf9db_40311"
+          },
+          {
+            description: "DASH",
+            acronym: "DASH",
+            url_image: "https://wiki.trezor.io/images/Dash.png"
+          }
+        ]
 
-Coin.create!(
-    [
-        { 
-          description: "Bitcoin",
-          acronym: "BTC",
-          url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
-        },   
-        {
-          description: "Ethereum",
-          acronym: "ETH",
-         url_image: "https://d2a4hncphh3gxw.cloudfront.net/image/artwork/40311/89/23/89231ba1d7cd720fb5d00106b33bf9db_40311"
-        },
-        {
-          description: "DASH",
-          acronym: "DASH",
-          url_image: "https://wiki.trezor.io/images/Dash.png"
-        }
-    ]
-)
+coins.each do |coin|
+    
+    Coin.find_or_create_by!(coin)    
+end
+
+spinner.success("Concluido!")
